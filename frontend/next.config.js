55cd798b51238377c865b61b3cadbc7e4acf7103
isaacalex007 <47508869+isaacalex007@users.noreply.github.com>
2025-07-29 15:53:50 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,10 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This is the definitive fix, using the standard Next.js configuration
-  // to allow all subdomains from our cloud development environment.
-  allowedDevOrigins: ['https://3000-firebase-haven-1753564061535.cluster-rhptpnrfenhe4qarq36djxjqmg.cloudworkstations.dev'],
-
+  experimental: {
+    // Replace with your Vercel URL
+    allowedDevOrigins: ["https://haven-project-tau.vercel.app"],
+  },
   typescript: {
-    // We'll keep this to prevent type errors from blocking our progress.
     ignoreBuildErrors: true,
   },
 };
